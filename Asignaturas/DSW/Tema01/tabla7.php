b162c940cf329fe7012b4eae3a349a2ae93169b1
vmcabreu <victormanueltoa@gmail.com>
2022-09-16 20:18:26 +0100

    Ejercicio Tabla 7

---
+++ Asignaturas/DSW/Tema01/tabla7.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <?php 
+            for ($i=1; $i < 10; $i++) { 
+                echo "<tr><td>7</td><td>*</td><td>$i</td><td> = </td><td>",(7*$i),"</td></tr>\n";
+            }
+        ?>
+    </table>
+    
+</body>
+</html>
