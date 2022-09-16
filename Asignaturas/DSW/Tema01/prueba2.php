3967c8ce996d2efe2df985d94d115b8dcd22d5b1
daw <daw@debian-BULLSEYE-live-builder-AMD64>
2022-09-16 18:09:36 +0100

    Actualizacion de las carpetas

---
+++ Asignaturas/DSW/Tema01/prueba2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Mi primer programa PHP</title>
+    </head>
+    <body>
+        <h1>Mi primer programa PHP</h1>
+        <?php
+            $a=1;
+            $b=2;
+            echo "La suma es: ",$a+$b;
+        ?>
+    </body>
+</html>
