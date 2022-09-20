cd54f38c982427c42e7b61b61a20b7125c1e27b5
vmcabreu <victormanueltoa@gmail.com>
2022-09-20 16:24:19 +0100

    Se hizo ejemplo del Formulario

---
+++ Asignaturas/DSW/Tema01/Formulario+PHP/sumaMedio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulario</title>
+    </head>
+    <body>
+        <?php
+        $n1=$_POST['n1'];
+        $n2=$_POST['n2'];
+        $suma=0;
+        for ($i=$n1; $i<=$n2 ; $i++) { 
+            $suma+=$i;
+        }
+        echo "Las suma vale: $suma.\n<br/>";
+        echo "La media es: ", $suma/($n2-$n1-1);
+        ?>
+        
+    </body>
+</html>
