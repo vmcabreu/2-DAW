a3cf3239c68092627f673928a3a7150f5bb1b702
Victor Manuel Cabrera Abreu <victormanueltoa@gmail.com>
2022-09-21 01:53:24 +0100

    Ejercicio 06 terminado

--- Asignaturas/DEW/Tema 02/AUT2_02 Variables/Ejercicio06.js
+++ Asignaturas/DEW/Tema 02/AUT2_02 Variables/Ejercicio06.js
@@ -1,15 +1,11 @@
 
-let numero = Number(prompt("Ejercicio 06: \n\n : Introduce un número: "));
-
-for (let repeticiones = 0; repeticiones < 6; repeticiones) {
-    let suma ;
+let nnatural = Number(prompt("Ejercicio 06: \n\n : Introduce un número: "));
+for (let repeticiones = 0; repeticiones < 6; repeticiones++) {
+    let suma5=nnatural;
     for (let index = 1; index < 6; index++) {
-        suma = numero + (numero+index);  
+        suma5+= index;
     }
-    console.log("La suma de "+n1+" y sus 5 números siguientes es : "+suma);  
+    console.log("La suma de "+nnatural+" y sus 5 números siguientes es : "+Number(suma5-1)); 
+
 }
-
-
-
-
 document.getElementById("resultado6").innerHTML = "Las sumas se han realizado en la consola del navegador. ";
