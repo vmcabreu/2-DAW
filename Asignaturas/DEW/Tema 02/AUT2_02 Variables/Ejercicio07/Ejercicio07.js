537124aa407b5e915a5dfe36f86fc41c4e191e63
vmcabreu <victormanueltoa@gmail.com>
2022-09-23 19:57:01 +0100

    Arreglos del Ejercicio 7

--- Asignaturas/DEW/Tema 02/AUT2_02 Variables/Ejercicio07/Ejercicio07.js
+++ Asignaturas/DEW/Tema 02/AUT2_02 Variables/Ejercicio07/Ejercicio07.js
@@ -2,7 +2,7 @@ function ejercicio7() {
  let resultado07="Lo siento ha fallado";
 let intentos=3;
 let letras=1;
-const paises=["Madrid","París","Venezuela","Londres","Viena","Berlin","Caracas","Buenos Aires","Brasilia","Mexico"]
+const paises=["Madrid","París","Londres","Viena","Berlin","Caracas","Buenos Aires","Brasilia","Montevideo"]
 const respuesta= paises[parseInt(Math.random() * 11)];
 do{
     let capital = prompt("Ejercicio 07:  \n\n Adivina la capital del país \n\n Intentos: "+intentos+" \n\n Pista: "+ respuesta.substring(0,4-intentos)+"\n\n Introduce tu respuesta: ");
