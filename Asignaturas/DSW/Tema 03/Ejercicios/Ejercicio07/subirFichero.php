18d1e8c5c30f990efe247570b087135e964e02c6
Victor Manuel Cabrera Abreu <victormanueltoa@gmail.com>
2022-10-10 02:06:21 +0100

    DSW - Realizados ejercicios 7 y 8

--- Asignaturas/DSW/Tema 03/Ejercicios/Ejercicio07/subirFichero.php
+++ Asignaturas/DSW/Tema 03/Ejercicios/Ejercicio07/subirFichero.php
@@ -1,21 +1,31 @@
 <?php
     if ($_POST) {
-        $fichero = $_FILES['fichero'];
+        $fichero = $_FILES['input'];
         $destination = "/var/www/phpdata/".$fichero['name'];
         if (file_exists($destination)) {
             echo "El fichero ".$fichero['name']." ya existe. ";
         }else{
             move_uploaded_file($fichero['tmp_name'],$destination);
             $archivo = fopen($destination,"r");
-            $linea = preg_split(";",fgets($archivo));
+            $pointer = 0;
             echo "<table>";
-            echo "<tr>";
-            while ($linea == true) {
-                echo "<td>";
-                echo "";
-                echo "</td>";
-            }
-            echo "</tr>";
+            
+            do{
+                $linea = fgets($archivo);
+                $pointer += count($linea);
+                $lineaArray = preg_split("/;/",$linea);
+                echo "<tr>";
+                for ($i=0; $i < count($lineaArray); $i++) { 
+                    echo "<td>";
+                    echo "$lineaArray[$i]";
+                    echo "</td>";
+                }
+                echo "</tr>";
+                fseek($archivo, $pointer+1);
+
+
+            }while ($linea == true)
+
             echo "</table>";
 
         }
---
+++ Asignaturas/DSW/Tema 03/Ejercicios/Ejercicio08/subirImagen.php
@@ -0,0 +1,13 @@
+<?php
+    if ($_POST) {
+        $fichero = $_FILES['input'];
+        $destination = "/var/www/phpdata/".$fichero['name'];
+        if (file_exists($destination)) {
+            echo "El fichero ".$fichero['name']." ya existe. ";
+        }else{
+            move_uploaded_file($fichero['tmp_name'],$destination);
+            $base64= base64_encode(file_get_contents($fichero));
+            echo '<img src="data: '.mime_content_type($fichero).';base64,'.$base64.'">';
+        }
+    }
+?>
