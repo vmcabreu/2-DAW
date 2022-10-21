ce8006665715684fb76b8d0e9d548d1d240dfc13
vmcabreu <victormanueltoa@gmail.com>
2022-10-21 18:54:48 +0100

    Ejercicio 02 BOM DEW Casi hecho

--- Asignaturas/DEW/Tema 02/AUT2_06_Primeros_BOM/Ejercicio02/Ejercicio2.js
+++ Asignaturas/DEW/Tema 02/AUT2_06_Primeros_BOM/Ejercicio02/Ejercicio2.js
@@ -6,7 +6,41 @@ Reducir: modificará el tamaño de la ventana disminuyendo tanto el ancho como e
 Mover: Ubicará la pantalla en la posición indicada en los cuadros de texto. Los valores tendrán que ser enteros positivos. Antes de mover la nueva pantalla tendrá que comprobar que la nueva posición de la ventana es posible teniendo en cuenta tanto la posición como su alto y ancho y el tamaño de la pantalla.
 muestra:*/
 
-let ventanaNueva;
+let userAgent = navigator.userAgent;
+let css = document.createElement('link');
+css.rel = "stylesheet";
+css.type = "text/css";
+if (userAgent.match(/chrome|chromium|crios/i)) {
+    css.href = "chrome.css"
+    document.getElementsByTagName("head")[0].appendChild(css);
+} else if (userAgent.match(/firefox|fxios/i)) {
+    css.href = "firefox.css"
+    document.getElementsByTagName("head")[0].appendChild(css);
+}
+
+let visitCount = Number(localStorage.getItem("page_view"));
+
+function firefox(){
+    let pantalla= document.createElement('div');
+    let ventana= document.createElement('div');
+    let idAtrPantalla=["altoD","anchoD","altoT","anchoT","profColor","resolucionPx"];
+    let idAtrVentana=["altoExt","anchoExt","cordX","cordY","visitas"];
+    pantalla.id = "pantalla";
+    ventana.id= "ventana";
+    //Crear un elemento para VEntana y Pantalla
+
+}
 
 
-document.getElementById("altoD").innerHTML = ""
+document.getElementById("altoD").innerHTML = "Alto disponible: "+screen.availHeight+" px.";
+document.getElementById("anchoD").innerHTML = "Ancho disponible: "+screen.availWidth+" px.";
+document.getElementById("altoT").innerHTML = "Alto total: "+screen.height+" px.";
+document.getElementById("anchoT").innerHTML = "Ancho total: "+screen.width+" px.";
+document.getElementById("profColor").innerHTML = "Profundidad de color: "+screen.colorDepth;
+document.getElementById("resolucionPx").innerHTML = "Resolución (bits por pixel): "+screen.pixelDepth;
+
+document.getElementById("altoExt").innerHTML = "Alto exterior: "+window.outerHeight+" px.";
+document.getElementById("anchoExt").innerHTML = "Ancho exterior: "+window.outerWidth+" px.";
+document.getElementById("cordX").innerHTML = "Coordenada X respecto a la pantalla: "+window.screenX+" px.";
+document.getElementById("cordY").innerHTML = "Coordenada Y respecto a la pantalla: "+window.screenY+" px.";
+document.getElementById("visitas").innerHTML = "Ha visitado: "+visitCount+" páginas";
