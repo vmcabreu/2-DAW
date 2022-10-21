148e206c553e9d75436e2db52269f0930bfc58d4
vmcabreu <victormanueltoa@gmail.com>
2022-10-21 19:10:12 +0100

    DSW - Ejercicio 1 T4 - Añadido el hash a la pass

--- Asignaturas/DSW/Tema 04/Ejercicios/Ejercicio1/ejercicio1.php
+++ Asignaturas/DSW/Tema 04/Ejercicios/Ejercicio1/ejercicio1.php
@@ -15,25 +15,25 @@ declare(strict_types=1);
 <body>
     <form action="" method="post" enctype="multipart/form-data">
         <label for="nombre">Nombre: </label><br>
-        <input type="text" id="nombre" name="nombre" /><br>
+        <input required="required" type="text" id="nombre" name="nombre" /><br>
         <label for="ap1">Apellidos 1: </label><br>
-        <input type="text" id="ap1" name="ap1" /> <br>
+        <input required="required" type="text" id="ap1" name="ap1" /> <br>
         <label for="ap1">Apellidos 2: </label><br>
         <input type="text" id="ap2" name="ap2" /> <br>
         <label for="user">Usuario: </label><br>
-        <input type="text" id="user" name="user" /> <br>
+        <input required="required" type="text" id="user" name="user" /> <br>
         <label for="email">Email: </label><br>
-        <input type="text" id="email" name="email" /> <br>
+        <input required="required" type="text" id="email" name="email" /> <br>
         <label for="pass">Contraseña: </label><br>
-        <input type="password" id="pass" name="pass" /> <br>
+        <input required="required" type="password" id="pass" name="pass" /> <br>
         <label for="passConfirm">Confirmar contraseña: </label><br>
-        <input type="password" id="passConfirm" name="passConfirm" /> <br>
+        <input required="required" type="password" id="passConfirm" name="passConfirm" /> <br>
         <button type="submit" value="Enviar" name="enviar">Registrarse</button><br><br>
         <a href="login.php">Ir al Login</a>
     </form>
     <?php
 
-    function verifyExistingUser(string $usuariojson, array $registro)
+    function verificarUsuario(string $usuariojson, array $registro)
     {
         $usuariof = (array)json_decode($usuariojson);
         return ($usuariof['user'] == $registro['user']);
@@ -41,28 +41,32 @@ declare(strict_types=1);
 
     if (isset($_POST["enviar"])) {
         $passConfirm = $_POST['passConfirm'];
-        $usuario = [
-            "nombre" => $_POST['nombre'], "ap1" => $_POST['ap1'], "ap2" => $_POST['ap2'],
-            "user" => $_POST['user'], "pass" => $_POST['pass'], "email" => $_POST['email']
-        ];
+        $passTxt = $_POST['pass'];
 
-        if ($usuario['pass'] != $passConfirm) {
+        if ($passTxt != $passConfirm) {
             echo "Las contraseñas no coinciden.";
             exit;
+        } else {
+            $passHash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+            $usuario = [
+                "nombre" => $_POST['nombre'], "ap1" => $_POST['ap1'], "ap2" => $_POST['ap2'],
+                "user" => $_POST['user'], "pass" => $passHash, "email" => $_POST['email']
+            ];
         }
 
-        $bbdd = "/var/www/phpdata/BDusuario.txt";
-        $fdbaseDatos = fopen($bbdd, "c+");
+
+        $BDatos = "/var/www/phpdata/BDusuario.txt";
+        $fdbaseDatos = fopen($BDatos, "c+");
         $header = fgets($fdbaseDatos);
         $usuarioJson = json_encode($usuario, JSON_UNESCAPED_UNICODE);
 
         if ($header == null) {
             fwrite($fdbaseDatos, $usuarioJson . "\n");
-            echo "Se ha registrado con éxito.";
+            echo "<script> alert('Se ha registrado con éxito.') </script>";
         } else {
             while ($linea = fgets($fdbaseDatos)) {
-                if (verifyExistingUser($linea, $usuario)) {
-                    echo "Este usuario ya está registrado.";
+                if (verificarUsuario($linea, $usuario)) {
+                    echo "<script> alert(Este usuario ya está registrado.') </script>";
                     exit;
                 }
             }
