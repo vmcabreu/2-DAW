d2f7b56d3c22be662497e3c7135f449b019c6de9
vmcabreu <victormanueltoa@gmail.com>
2022-10-26 19:30:12 +0100

    DEW - AUT2_08_formularios

---
+++ Asignaturas/DEW/Tema 02/AUT2_08_formularios/main.js
@@ -0,0 +1,10 @@
+const tr = document.createElement("tr");
+const tdLabel = document.createElement("td");
+const tdInput = document.createElement("td");
+tr.appendChild(tdLabel);
+tr.appendChild(tdInput);
+const label = document.createElement("label");
+label.setAttribute("for", "ciclo");
+label.nodeValue();
+document.getElementById("boton").style.display = "none";
+updateRows();
