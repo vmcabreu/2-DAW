29d65cbbd14652cbda05915c66c5b12560e68ab5
vmcabreu <victormanueltoa@gmail.com>
2022-11-16 19:39:36 +0000

    DEW - Ahorcado ya funcional

--- Asignaturas/DEW/Tema 02/Proyecto_Ahorcado/script.js
+++ Asignaturas/DEW/Tema 02/Proyecto_Ahorcado/script.js
@@ -2,20 +2,21 @@ let palabra = "Parque Jurasico";
 let palabras = palabra.toUpperCase().split(" ");
 let incognita = [];
 let letras = [];
-let vidas = 3;
+let numVidas = Number(document.getElementById("vidas").textContent.charAt(7));
 let pista = palabra.replaceAll(/\w\d/g, "_");
+
+console.log(numVidas);
 
 const pistas = document.getElementById("palabra");
 pistas.addEventListener("input", mostrarLetra);
-/*
-const victoria = document.getElementById("victoria");
-pistas.addEventListener("input", comprobarVictoria);
-*/
+
+
+
 /**
- * Toma las palabras en la matriz y las divide en letras, luego muestra las letras en el HTML.
- * También muestra el teclado interactivo para poder jugar al juego.
+ * Toma las palabras de la matriz y las coloca en la matriz de incógnita, que es la que se mostrará en
+ * la pantalla.
  */
-function desglosePalabra() {
+function empezarPartida() {
   for (let i = 0; i < palabras.length; i++) {
     for (let j = 0; j < palabras[i].length; j++) {
       letras.push(palabras[i].charAt(j));
@@ -26,57 +27,82 @@ function desglosePalabra() {
   }
   mostrarLetra();
   document.getElementById("empezar").classList.add("ocultar");
-  document.getElementById("teclado").classList.remove("ocultar");
+  document.getElementById("jugadores").classList.add("ocultar");
+  document.getElementById("teclado1").classList.remove("ocultar");
+  document.getElementById("vidas").classList.remove("ocultar");
 }
 
+
+/**
+ * Toma la matriz de letras y las coloca en una cadena, luego coloca esa cadena en el elemento HTML con
+ * el id "palabra".
+ */
 function mostrarLetra() {
   pista = "";
   incognita.forEach((element) => {
     pista += element;
     document.getElementById("palabra").innerHTML = pista;
-    comprobarVictoria();
-    comprobarDerrota();
   });
+  comprobarVictoria();
 }
 
 /**
- * Comprueba si la letra en la que hizo clic está en la palabra y, si lo está, la agrega a la palabra.
- * Si no está el jugador pierde una vida
+ * Comprueba si la letra está en la palabra, si lo está, la suma a la palabra oculta, si no lo está, le
+ * resta una vida
  * @param letra - la letra en la que el usuario ha hecho clic
  */
 function comprobarLetra(letra) {
-  for (let i = 0; i < letras.length; i++) {
-    if (letra == letras[i]) {
-      if (letras[i - 1] == " " || i == 0) {
-        incognita[i] = letra.toUpperCase();
-        mostrarLetra();
-      } else {
-        incognita[i] = letra.toLowerCase();
-        mostrarLetra();
+  let letrasmayus =palabra.toUpperCase();
+  if (letrasmayus.includes(letra)) {
+    for (let i = 0; i < letras.length; i++) {
+      if (letra == letras[i]) {
+        if (letras[i - 1] == " " || i == 0) {
+          incognita[i] = letra.toUpperCase();
+          mostrarLetra();
+          document.getElementById(letra).classList.add("transparente");
+          document.getElementById(letra).disabled = true;
+        } else {
+          incognita[i] = letra.toLowerCase();
+          mostrarLetra();
+          document.getElementById(letra).classList.add("transparente");
+          document.getElementById(letra);
+        }
       }
-      document.getElementById(letra).classList.add("transparente");
-      document.getElementById(letra);
-    } else {
-      vidas--;
     }
+  } else {
+    numVidas--;
+    document.getElementById("vidas").innerHTML = "Vidas: " + numVidas;
+    comprobarDerrota();
   }
 }
 
 /**
- * Comprueba si la palabra que has adivinado es la misma que la palabra que intentas adivinar
+ * Comprueba si la palabra adivinada por el usuario es la misma que la palabra a adivinar. Si es así,
+ * muestra un mensaje que dice que el usuario ha ganado y muestra el botón para reiniciar el juego.
  */
 function comprobarVictoria() {
   if (pistas.textContent == palabra) {
     document.getElementById("victoria").innerHTML = "Has ganado";
+    document.getElementById("reiniciar").classList.remove("ocultar");
   }
 }
 
 /**
- * Comprueba si el jugador ha perdido el juego.
+ * Comprueba si el número de vidas es 0 y, de ser así, oculta el teclado, muestra el botón de reinicio
+ * y muestra la palabra que el jugador estaba tratando de adivinar.
  */
 function comprobarDerrota() {
-  if (vidas <= 0) {
+  if (numVidas == 0) {
+    document.getElementById("teclado1").classList.add("ocultar");
+    document.getElementById("reiniciar").classList.remove("ocultar");
     document.getElementById("victoria").innerHTML =
       "Has perdido. La palabra era: " + palabra;
   }
+}
+
+/**
+ * Cuando se hace clic en el botón, vuelve a cargar la página.
+ */
+function reiniciarPartida() {
+  window.location.reload();
 }
