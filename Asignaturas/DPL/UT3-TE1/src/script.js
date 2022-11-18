1f75a5f5d5497cc7e3ae752a0cbd9a4ec6d914a7
vmcabreu <victormanueltoa@gmail.com>
2022-11-18 17:02:52 +0000

    DPL

--- Asignaturas/DPL/UT3-TE1/src/script.js
+++ Asignaturas/DPL/UT3-TE1/src/script.js
@@ -2,7 +2,6 @@ const color = document.getElementById("borderColor");
 const contenedorImgs = document.getElementById("imagenes");
 const form = document.getElementById('form');
 form.addEventListener('submit', (e) => e.preventDefault());
-
 for (let i = 1; i <= 20; i++) {
     let img = document.createElement('img');
     if (i < 10) {
@@ -17,20 +16,6 @@ for (let i = 1; i <= 20; i++) {
     }
 }
 
-/**
- * Comprueba si el valor del campo de entrada coincide con la expresión regular. Si es así, devuelve
- * verdadero, de lo contrario, devuelve falso
- * @returns un valor booleano.
- */
-function validarHex() {
-    const regExpHex = /^#([A-F0-9]{3}){1,2}/i;
-    if (regExpHex.test(color.value)) {
-        return true;
-    } else {
-        alert("Color no válido. Solo admite formato hexadecimal");
-        return false;
-    }
-}
 
 function generarImagen() {
     contenedorImgs.innerHTML = ' ';
