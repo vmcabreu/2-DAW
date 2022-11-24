dc803d0e6fba3f13d446e3d28a20f24e5ddad409
Victor Manuel Cabrera Abreu <victormanueltoa@gmail.com>
2022-11-24 05:09:01 +0000

    DEW - Doc creado

--- Asignaturas/DPL/UT3-TE1/src/script.js
+++ Asignaturas/DPL/UT3-TE1/src/script.js
@@ -1,12 +1,13 @@
-const tamanio = document.getElementById("tamaño");
+const imgSize = document.getElementById("size");
 const anchoBorde = document.getElementById("borderWidth");
-const color = document.getElementById("borderColor");
+const colorBorde = document.getElementById("borderColor");
 const enfoqueSigma = document.getElementById('enfoqueSigma');
-const enfoqueRadio = document.getElementById('enfoque');
+const enfoqueRadio = document.getElementById('enfoqueRadio');
 const desenfoqueSigma = document.getElementById('desenfoqueSigma');
 const desenfoqueRadio = document.getElementById('desenfoqueRadio');
 const contenedorImgs = document.getElementById("imagenes");
 const form = document.getElementById('form');
+
 form.addEventListener('submit', (e) => e.preventDefault());
 for (let i = 1; i <= 20; i++) {
     let img = document.createElement('img');
@@ -25,22 +26,24 @@ for (let i = 1; i <= 20; i++) {
 
 function generarImagen() {
     contenedorImgs.innerHTML = ' ';
+    editarImagen();
 }
 
 
-function editarImagen(){
+function editarImagen() {
+    let color = colorBorde.value.replace("#", "");
     for (let i = 1; i <= 20; i++) {
         let img = document.createElement('img');
         if (i < 10) {
-            img.src = "../imgs/image0" + i + ".jpg?dw="+tamanio+"&dh="+tamanio+"&bw="+anchoBorde+"&bc="+color+"&sharpen="+enfoqueRadio+","+enfoqueSigma+"&blur="+desenfoqueRadio+","+desenfoqueSigma;
+            img.src = "../imgs/image0" + i + ".jpg?dw=" + imgSize.value + "&dh=" + imgSize.value + "&bw=" + anchoBorde.value + "&bh=" + anchoBorde.value + "&bc=" + color + "&sharpen=" + enfoqueRadio.value + "x" + enfoqueSigma.value + "&blur=" + desenfoqueRadio.value + "x" + desenfoqueSigma.value;
             img.classList.add("col-3");
             contenedorImgs.appendChild(img);
-    
+
         } else {
-            img.src = "../imgs/image" + i + ".jpg?dw="+tamanio+"&dh="+tamanio+"&bw="+anchoBorde+"&bc="+color+"&sharpen="+enfoqueRadio+","+enfoqueSigma+"&blur="+desenfoqueRadio+","+desenfoqueSigma;
+            img.src = "../imgs/image" + i + ".jpg?dw=" + imgSize.value + "&dh=" + imgSize.value + "&bw=" + anchoBorde.value + "&bh=" + anchoBorde.value + "&bc=" + color + "&sharpen=" + enfoqueRadio.value + "x" + enfoqueSigma.value + "&blur=" + desenfoqueRadio.value + "x" + desenfoqueSigma.value;
             img.classList.add("col-3");
-            contenedorImgs.replaceChild(img);
+            contenedorImgs.appendChild(img);
         }
-}
+    }
 
 }
