91fdae068890d3f326a855d38b7338e122b760c4
vmcabreu <victormanueltoa@gmail.com>
2022-12-02 16:24:24 +0000

    DPL - Comenzando ejercicio T4

---
+++ Asignaturas/DPL/UT4-TE1/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('__CONFIG__',true);
+    define('HOSTDB','alu6618.arkania.es');
+    define("PORT_DB","5432");
+    define('DATABASE','travelroad');
+    define('DB_USER','travelroad_user');
+    define("DB_PASS","dpl6618");
+
