a527dabceaf397fd17e4faf5162c0895c4bc5ce4
vmcabreu <victormanueltoa@gmail.com>
2022-12-05 17:38:08 +0000

    DEW - JQuery Script finalizado

--- Asignaturas/DEW/PrimerTrimestre/JQUERY/script.js
+++ Asignaturas/DEW/PrimerTrimestre/JQUERY/script.js
@@ -4,7 +4,6 @@ let productos = $.getJSON("productos.json", function () {
   $("#articulos").css("display", "none").delay(4000).fadeIn(400);
 })
   .done(function (datos) {
-    console.log("second success");
     let i = 0;
     $.each(datos, function (clave, valor) {
       $("<div>")
@@ -18,7 +17,7 @@ let productos = $.getJSON("productos.json", function () {
             valor.abrev +
             ">" +
             valor.nombre +
-            " <span id=precio" +
+            "<span id=precio" +
             valor.abrev +
             "> - " +
             valor.precio +
@@ -32,10 +31,75 @@ let productos = $.getJSON("productos.json", function () {
     console.log("error");
   })
   .always(function () {
-    console.log("complete");
   });
 
-  function agregarCesta(){
-   console.log($("input:checked"));
+  let igictotal;
+  let importeTotal= 0;
+
+/**
+ * Añade los artículos seleccionados al carrito de la compra.
+ */
+  $("#agregarCarrito").click(function() {
+   $.each($("input:checked"),function(clave, valor){
+    let articulo = $(valor).next().text();
+    let datos = articulo.split("-");
+    let precio = Number(datos[1].substring(1,datos[1].length - 2))
+    let articuloigic = precio - ((precio * 1.07) - precio)
+    importeTotal += articuloigic;
+    igictotal = (importeTotal * 1.07) - importeTotal;
+    $("<div>")
+        .append(
+          $("<p>")
+          .append(
+            datos[0] + " - " + articuloigic.toFixed(2) + " €"
+          )
+        ).appendTo("#cesta")
+   })
+   $("#igic").replaceWith("<span id='igic'>"+igictotal.toFixed(2)+"</span>")
+   $("#total").replaceWith("<span id='total'>"+importeTotal.toFixed(2)+"</span>")
     
-  }
+  })
+
+/**
+ * Es una función que refresca la lista de productos
+ */
+  $("#refrescarLista").click(function() {
+    $("#listaArticulos").empty();
+    let listaNueva = $.getJSON("productostest.json", function () {
+
+      $("#loading").fadeIn(400).delay(3500).fadeOut(500).delay(500);
+      $("#articulos").css("display", "none").delay(4000).fadeIn(400);
+    })
+      .done(function (datos) {
+        let i = 0;
+        $.each(datos, function (clave, valor) {
+          $("<div>")
+            .append(
+              $("<input>")
+                .attr("type", "checkbox")
+                .attr("id", "articulo" + i)
+            )
+            .append(
+              " <span id=" +
+                valor.abrev +
+                ">" +
+                valor.nombre +
+                "<span id=precio" +
+                valor.abrev +
+                "> - " +
+                valor.precio +
+                " €</span></span>"
+            )
+            .appendTo("#listaArticulos");
+          i++;
+        });
+      })
+      .fail(function () {
+        console.log("error");
+      })
+      .always(function () {
+    
+      });
+     
+   })
+  
