d392ddfec28d730b2d0049cc4185b955107fdbb1
Victor Manuel Cabrera Abreu <victormanueltoa@gmail.com>
2022-12-08 20:03:53 +0000

    DEW - JQUERY Terminado

--- Asignaturas/DEW/PrimerTrimestre/JQUERY/script.js
+++ Asignaturas/DEW/PrimerTrimestre/JQUERY/script.js
@@ -1,16 +1,21 @@
+
 let productos = $.getJSON("productos.json", function () {
 
   $("#loading").fadeIn(400).delay(3500).fadeOut(500).delay(500);
+  $("#titulo").css("display", "none").delay(4000).fadeIn(400);
   $("#articulos").css("display", "none").delay(4000).fadeIn(400);
 })
   .done(function (datos) {
     let i = 0;
     $.each(datos, function (clave, valor) {
       $("<div>")
+      .attr("class","form-check")
         .append(
           $("<input>")
             .attr("type", "checkbox")
+            .attr("class", "form-check-input")
             .attr("id", "articulo" + i)
+
         )
         .append(
           " <span id=" +
@@ -35,40 +40,16 @@ let productos = $.getJSON("productos.json", function () {
 
   let igictotal;
   let importeTotal= 0;
-
-/**
- * Añade los artículos seleccionados al carrito de la compra.
- */
-  $("#agregarCarrito").click(function() {
-   $.each($("input:checked"),function(clave, valor){
-    let articulo = $(valor).next().text();
-    let datos = articulo.split("-");
-    let precio = Number(datos[1].substring(1,datos[1].length - 2))
-    let articuloigic = precio - ((precio * 1.07) - precio)
-    importeTotal += articuloigic;
-    igictotal = (importeTotal * 1.07) - importeTotal;
-    $("<div>")
-        .append(
-          $("<p>")
-          .append(
-            datos[0] + " - " + articuloigic.toFixed(2) + " €"
-          )
-        ).appendTo("#cesta")
-   })
-   $("#igic").replaceWith("<span id='igic'>"+igictotal.toFixed(2)+"</span>")
-   $("#total").replaceWith("<span id='total'>"+importeTotal.toFixed(2)+"</span>")
-    
-  })
-
 /**
  * Es una función que refresca la lista de productos
- */
+*/
   $("#refrescarLista").click(function() {
     $("#listaArticulos").empty();
-    let listaNueva = $.getJSON("productostest.json", function () {
-
-      $("#loading").fadeIn(400).delay(3500).fadeOut(500).delay(500);
-      $("#articulos").css("display", "none").delay(4000).fadeIn(400);
+    let listaNueva = $.getJSON("productosActualizados.json", function () {
+      $("#articulos").css("display", "none").fadeOut(450);
+      $("#loading").fadeIn(400).delay(3000).fadeOut(450);
+      $("#titulo").css("display", "none").delay(4000).fadeIn(400);
+      $("#articulos").css("display", "none").delay(4000).fadeIn(450);
     })
       .done(function (datos) {
         let i = 0;
@@ -100,6 +81,31 @@ let productos = $.getJSON("productos.json", function () {
       .always(function () {
     
       });
-     
+  }
+  );
+
+/**
+ * Añade los artículos seleccionados al carrito de la compra.
+ */
+  $("#agregarCarrito").click(function() {
+   $.each($("input:checked"),function(clave, valor){
+    let articulo = $(valor).next().text();
+    let datos = articulo.split("-");
+    let precio = Number(datos[1].substring(1,datos[1].length - 2))
+    let articuloigic = precio - ((precio * 1.07) - precio)
+    importeTotal += articuloigic;
+    igictotal = (importeTotal * 1.07) - importeTotal;
+    $("<div>")
+        .append(
+          $("<p>")
+          .append(
+            datos[0] + " - " + articuloigic.toFixed(2) + " €"
+          )
+        ).appendTo("#cesta")
    })
-  
+   $("#igic").replaceWith("<span id='igic'>"+igictotal.toFixed(2)+"</span>")
+   $("#total").replaceWith("<span id='total'>"+importeTotal.toFixed(2)+"</span>")
+    
+  })
+
+
