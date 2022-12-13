f494609af295cff96bb3a9a2f301742954a18ce6
vmcabreu <victormanueltoa@gmail.com>
2022-12-13 20:01:03 +0000

    DSW - Proyecto Cartas

--- Asignaturas/DSW/ProyectoCartas/Baraja.php
+++ Asignaturas/DSW/ProyectoCartas/Baraja.php
@@ -1,47 +1,48 @@
 <?php
 require_once("Carta.php");
 
-class Baraja{
-    private $palos =  ["bastos","copas","espadas","oros"];
-    private $numeros= [1,2,3,4,5,6,7,8,9,10,11,12];
+class Baraja
+{
+    private $palos =  ["bastos", "copas", "espadas", "oros"];
+    private $numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     private $baraja = [];
 
-    public function __construct(Array $baraja = [])
+    public function __construct(array $baraja = [])
     {
         $this->baraja = $baraja;
     }
 
-    public function crearBaraja(int $numParejas){
+    public function crearBaraja(int $numParejas)
+    {
         if ($numParejas % 2 == 0) {
-            for ($i=0; $i < $numParejas ; $i++) { 
-                $paloCarta = $this->palos[random_int(0,3)];
-                $numCarta = $this->numeros[random_int(0,11)];
+            for ($i = 0; $i < $numParejas; $i++) {
+                $paloCarta = $this->palos[random_int(0, 3)];
+                $numCarta = $this->numeros[random_int(0, 11)];
                 $imgCarta = "$paloCarta$numCarta.png";
-                $carta = new Carta($paloCarta,$numCarta,$imgCarta);
-                if (!in_array($carta,$this->baraja)) {
-                    array_push($this->baraja,$carta,$carta);
-                }else{
+                $carta = new Carta($paloCarta, $numCarta, $imgCarta);
+                if (!in_array($carta, $this->baraja)) {
+                    array_push($this->baraja, $carta, $carta);
+                } else {
                     $i--;
                 }
             }
-        }else{
+        } else {
             echo "El número de parejas debe ser par";
         }
-
     }
 
-    public function crearMesa(){
+    public function crearMesa()
+    {
         $mesa = [];
         if (count($this->baraja) > 0) {
             foreach ($this->baraja as $carta) {
-                $name = $carta->palo.$carta->numero;
-                 array_push($mesa,"<div name='$name' style='display: inline-flex;' onclick=''><img src='$carta->bocabajo'></div>");
+                $name = $carta->palo . $carta->numero;
+                array_push($mesa, "<div><img src='$carta->bocabajo' name='$name'></div>");
             }
             shuffle($mesa);
-            for ($i=0; $i < count($mesa); $i++) { 
+            for ($i = 0; $i < count($mesa); $i++) {
                 echo $mesa[$i];
             }
         }
     }
 }
-?>
--- Asignaturas/DSW/ProyectoCartas/index.php
+++ Asignaturas/DSW/ProyectoCartas/index.php
@@ -11,22 +11,41 @@ require_once("Baraja.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Juego de Parejas</title>
+    <style>
+        div {
+            display: inline-flex;
+        }
+        img{
+            width: 192px;
+            height: 308px;
+        }
+    </style>
 </head>
-
 <body>
     <form method="post" action="#" enctype="multipart/form-data">
         <label for="nParejas">Introduce el número de parejas:</label><br>
         <input type="text" name="numParejas" id="nParejas" />
         <button type="submit">Crear Mesa</button>
     </form>
-        <?php
-        if (isset($_POST["numParejas"])) {
-            $nParejas = intval($_POST["numParejas"]);
-            $baraja = new Baraja();
-            $baraja->crearBaraja($nParejas);
-            $baraja->crearMesa();
-        }
-        ?>
+    <div id="mesa" style="display: block !important;">
+    <?php
+    if (isset($_POST["numParejas"])) {
+        $nParejas = intval($_POST["numParejas"]);
+        $baraja = new Baraja();
+        $baraja->crearBaraja($nParejas);
+        $baraja->crearMesa();
+    }
+    ?>
+    </div>
+    <script>
+        document.addEventListener("click", function(div){ 
+            div.target.src = `http://localhost/DSW/ProyectoCartas/imagenesBaraja/${div.target.name.replace(/\d+/,"")}/${div.target.name}.png`;
+            const mesa = document.getElementById("mesa");
+            for (const child of mesa.children) {
+                console.log(child.childNodes[0]);
+            }
+        })
+    </script>
 </body>
 
 </html>
