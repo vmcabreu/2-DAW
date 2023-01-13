56dcd62c0f611d108223195b613414993088cf87
vmcabreu <victormanueltoa@gmail.com>
2023-01-13 16:46:59 +0000

    DEW - Ejercicio React

--- Asignaturas/DEW/SegundoTrimestre/AUT4_02_Práctica Conversor monedas (React)/conversor/src/App.js
+++ Asignaturas/DEW/SegundoTrimestre/AUT4_02_Práctica Conversor monedas (React)/conversor/src/App.js
@@ -1,9 +1,56 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from 'react';
+
 
 function App() {
-  const conversion = (moneda1,moneda2,cantidad) => {
+  const [Euro, setEuro] = useState(false);
+  const [Dolar, setDolar] = useState(false);
+  const [Yen, setYen] = useState(false);
+  const eleccion1 = "";
+  const eleccion2 = "";
+  const [cantidad, setCantidad] = useState(0);
 
+  const conseguirCantidad= (e) => {
+    setCantidad(e.target.value);
+  };
+
+  function seleccionarMoneda(e) {
+    const selectedValue = e.target.value;
+    if (selectedValue === "Euro") {
+      setEuro(true);
+    } else if (selectedValue === "Dolar") {
+      setDolar(true);
+    } else if (selectedValue === "Yen") {
+      setYen(true);
+    }
+    if (e.target.name == "moneda1") {
+      eleccion1 = selectedValue;
+    } else {
+      eleccion2 = selectedValue;
+    }
+  }
+
+  function conversionMoneda() {
+    let conversion = 0;
+    if (eleccion1 == "Euro" && eleccion2 == "Dolar") {
+      conversion = (cantidad * 0.90) / 1;
+      return "La conversión de "+cantidad+" "+eleccion1+" a "+eleccion2+" son: "+conversion;
+    } else if (eleccion1 == "Euro" && eleccion2 == "Yen") {
+      conversion = (cantidad * 138) / 1;
+      return "La conversión de "+cantidad+" "+eleccion1+" a "+eleccion2+" son: "+conversion;
+    } else if (eleccion1 == "Dolar" && eleccion2 == "Euro") {
+      conversion = cantidad / 0.90;
+      return "La conversión de "+cantidad+" "+eleccion1+" a "+eleccion2+" son: "+conversion;
+    } else if (eleccion1 == "Dolar" && eleccion2 == "Yen") {
+      conversion = (cantidad * 138) / 0.90;
+      return "La conversión de "+cantidad+" "+eleccion1+" a "+eleccion2+" son: "+conversion;
+    } else if (eleccion1 == "Yen" && eleccion2 == "Dolar") {
+      conversion = (cantidad * 0.9) / 138;
+      return "La conversión de "+cantidad+" "+eleccion1+" a "+eleccion2+" son: "+conversion; 
+    } else if (eleccion1 == "Yen" && eleccion2 == "Euro") {
+      conversion = (cantidad) / 138;
+      return "La conversión de "+cantidad+" "+eleccion1+" a "+eleccion2+" son: "+conversion;
+    }
   }
 
   return (
@@ -11,22 +58,22 @@ function App() {
       <header className="App-header">
         <h1>Conversor Moneda</h1>
         <div>
-        <label>Convierte de </label>
-        <select>
-          <option selected>Euro</option>
-          <option>Dólar</option>
-          <option>Yen</option>
-        </select>
-        <label> a </label>
-        <select>
-          <option>Euro</option>
-          <option selected>Dólar</option>
-          <option>Yen</option>
-        </select>
+          <label>Convierte de </label>
+          <select onChange={seleccionarMoneda(e)}>
+            <option name="moneda1">{Euro}</option>
+            <option name="moneda1">{Dolar}</option>
+            <option name="moneda1">{Yen}</option>
+          </select>
+          <label> a </label>
+          <select onChange={seleccionarMoneda(e)}>
+            <option name="moneda2">{Euro}</option>
+            <option name="moneda2">{Dolar}</option>
+            <option name="moneda2">{Yen}</option>
+          </select>
         </div>
         <label>Cantidad: </label>
-        <input type="number" value="0"></input>
-        <button onClick={() => conversion("Goal!")}>Convertir</button>
+        <input type="number" onChange={conseguirCantidad} value={cantidad}></input>
+        <button onClick={(e) => conversionMoneda()}>Convertir</button>
       </header>
     </div>
   );
