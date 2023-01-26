5775823a99fb4fa3d0b13101dcb10d2c9c64a7e3
vmcabreu <tomasnahuelantela@gmail.com>
2023-01-26 19:29:18 +0000

    DEW - Proyecto Marvel

--- Asignaturas/DEW/SegundoTrimestre/AEUT4_04_Marvel_films/marvel_films/src/app/modelo/mock/mock.mock.ts
+++ Asignaturas/DEW/SegundoTrimestre/AEUT4_04_Marvel_films/marvel_films/src/app/modelo/mock/mock.mock.ts
@@ -8,11 +8,4 @@ import { Observable } from 'rxjs';
 
 
 export class Mock {
-    url = 'https://www.qando.es/docs/films.php';
-
-    constructor(private http: HttpClient) { }
-
-    getUsers(): Observable {
-        return this.http.get(this.url + '/users');
-    }
 }
